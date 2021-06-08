9ee0eac74da629183e05274cf2d8a87eb618fae6
ZafriMahendraPutra007 <85572027+ZafriMahendraPutra007@users.noreply.github.com>
2021-06-08 23:19:29 +0700

    Add files via upload

---
+++ class2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overriding;
+
+/**
+ *
+ * @author zafri
+ */
+    
+public class class2 extends induk{
+    
+    public static void main(String[] args){
+        class2 data = new class2();
+System.out.println("Umur Abu    : "+data.ambilA());
+System.out.println("Umur Bashar : "+data.ambilB());
+    }
+}
+
---
+++ induk.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overriding;
+
+/**
+ *
+ * @author zafri
+ */
+public class induk {
+   int A=19;
+   int B=30; 
+    
+    int ambilA(){
+        return A;
+    }
+    int ambilB(){
+        return B;
+    }
+}
